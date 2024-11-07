5613cc463f3004d310cbe2a27c5a560856872803
Geraldo Duarte <geraldoduartedemedeirosneto@gmail.com>
2024-11-06 21:56:10 -0300

    Add files via upload

--- mercadinho/debitos.php
+++ mercadinho/debitos.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fornecedores</title>
+    <title>Débitos</title>
 </head>
 <body>
-    <h1>Fornecedores</h1>
+    <h1>Débitos</h1>
     <a href="index.php"><button>Voltar ao Menu</button></a>
 </body>
 </html>
