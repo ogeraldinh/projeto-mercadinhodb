2f037cb3f9a0476a3d23fd43a62c3e47a3d06e9b
Maria Lígia <ligianascimento002@gmail.com>
2024-11-09 17:25:54 -0300

    Update estoque.php
    agr aparece o nome do fornecedor

--- mercadinho/estoque.php
+++ mercadinho/estoque.php
@@ -1,15 +1,21 @@
 <?php
     session_start();
     include('config.php');
-    if((!isset($_SESSION['id_usuario']) == true) and (!isset($_SESSION['senha_usuario']) == true)) {
+    if ((!isset($_SESSION['id_usuario']) == true) and (!isset($_SESSION['senha_usuario']) == true)) {
         unset($_SESSION['id_usuario']);
         unset($_SESSION['senha_usuario']);
         header('Location: login.php');
+        exit();
     }
 
     $logado = $_SESSION['id_usuario'];
-    $sql = "SELECT * FROM produto ORDER BY nome_produto";
-    $result = $conexao -> query($sql);
+    
+    // Alteração da consulta para fazer o JOIN entre produto e fornecedor
+    $sql = "SELECT produto.id_produto, produto.nome_produto, produto.estoque, fornecedor.nome 
+            FROM produto
+            JOIN fornecedor ON produto.id_fornecedor = fornecedor.id_fornecedor
+            ORDER BY produto.nome_produto";
+    $result = $conexao->query($sql);
 
     // print_r($result);
 ?>
@@ -21,6 +27,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/estoque.css">
     <title>Estoque</title>
+    <script>
+        function confirmarExclusao(id) {
+            if (confirm("Tem certeza que deseja excluir este produto?")) {
+                window.location.href = "excluir_estoque.php?id=" + id;0
+            }
+        }
+    </script>
 </head>
 <body>
     <h1>Estoque</h1>
@@ -37,16 +50,18 @@
         </thead>
         <tbody>
             <?php
-                while($user_data = mysqli_fetch_assoc($result)) {
+                // Loop para exibir os produtos com o nome do fornecedor
+                while ($user_data = mysqli_fetch_assoc($result)) {
                     echo "<tr>";
                     echo "<td>".$user_data['id_produto']."</td>";
                     echo "<td>".$user_data['nome_produto']."</td>";
                     echo "<td>".$user_data['estoque']."</td>";
-                    echo "<td>".$user_data['id_fornecedor']."</td>";
+                    echo "<td>".$user_data['nome']."</td>";  // Exibe o nome do fornecedor
                     echo "<td>
                         <a href='editar_estoque.php?id=$user_data[id_produto]'><buttom>Editar</button></a>
-                        <a href='#'><buttom>Excluir</button></a>
+                         <a href='#' onclick='confirmarExclusao(".$user_data['id_produto'].")' style='color: red;'>Excluir</a>
                     </td>";
+                    
                 }
             
             ?>
@@ -56,5 +71,7 @@
 
 
     <a href="index.php"><button>Voltar ao Menu</button></a>
+    <a href="adicionar_produto.php"><button>adicionar produto</button></a>
+    
 </body>
 </html>
