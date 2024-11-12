8e9fe6f34742090de3bb17eec9e048daea7369b0
lin <laurasc1603@gmail.com>
2024-11-11 23:08:07 -0300

    favicon

--- mercadinho/vendas.php
+++ mercadinho/vendas.php
@@ -95,6 +95,7 @@ $result_clientes = $conexao->query($sql_clientes);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="assets/css/cadastro.css">
     <title>Registrar Venda</title>
 </head>
